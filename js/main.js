aacc1e8b37226c1ccdf003690744ad0f4cd07e11
nhbong <93415098+nhbong@users.noreply.github.com>
2021-10-30 10:11:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/TAY2.jpg',
+        imageUrl: 'img/IMG_1618816844756_1618817003638.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/TAY2.jpg")',
